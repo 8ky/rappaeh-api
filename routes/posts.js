ce84702e304fecb35d2c2f46913cf67f20b9c7a5
Hélio Zumba <helio.zumba@digitalfactory.co.ao>
2020-09-20 14:50:22 +0100

    deprecated function alter

--- routes/posts.js
+++ routes/posts.js
@@ -30,7 +30,7 @@ router.get('/:postId', async (req, res) => {
 
 router.delete('/:postId', async (req, res) => {
   try {
-    const removedPost = await Post.remove({
+    const removedPost = await Post.deleteOne({
       _id: req.params.postId
     })
     res.json(removedPost)
